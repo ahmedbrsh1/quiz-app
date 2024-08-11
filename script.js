9e80ec449f604f5bb2d425cff9666d3b90ce6746
Ahmed Ashraf <ahmedbrsh@hotmail.com>
2024-08-11 17:14:22 +0300

    submit on time runout

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ startBtn.addEventListener("click",function () {
     timer.style.display = "block";
     submitBtn.style.display = "block";
     getQuestions();
-    countdown(60);
+    countdown(30);
     
 })
 
@@ -28,43 +28,7 @@ function getQuestions() {
         if (this.readyState == 4 && this.status == 200){
             let questions = JSON.parse(this.responseText);
             gradeBox.lastElementChild.innerText = questions.length;
-            for (let i = 0; i < questions.length; i++) {
-                let questionBox = document.createElement("div");
-                questionBox.classList.add("question");
-                let questionNumber = document.createElement("div");
-                questionNumber.classList.add("question-number");
-                questionNumber.innerHTML = `Question ${i+1}`
-                questionBox.appendChild(questionNumber);
-                let questionTitle = document.createElement("div");
-                let questionTitleText = document.createTextNode(questions[i].title);
-                questionTitle.appendChild(questionTitleText);
-                questionTitle.classList.add("question-title");
-                questionBox.appendChild(questionTitle);                
-                for (let j = 1; j <= 4; j++) {
-                    let questionAnswer = document.createElement("div");
-                    questionAnswer.dataset.answer = questions[i].right_answer;
-                    questionAnswer.classList.add("question-answer");
-                    let radioInput = document.createElement("input");
-                    radioInput.setAttribute("type","radio");
-                    let dataId = document.createAttribute("data-id");
-                    // radioInput.id = `answer_${j}`;
-                    radioInput.setAttribute("data-id",`answer_${j}`);
-                    radioInput.name = `question_${i}`;
-                    
-                    let inputLabel = document.createElement("label");
-                    // inputLabel.setAttribute("for",`answer_${j}`);
-                    inputLabelText = document.createTextNode(questions[i][`answer_${j}`]);
-                    inputLabel.appendChild(radioInput);
-                    inputLabel.appendChild(inputLabelText);
-                    
-                    // questionAnswer.appendChild(radioInput);
-                    questionAnswer.appendChild(inputLabel);
-                    questionBox.appendChild(questionAnswer); 
-                }
-
-                container.insertBefore(questionBox,submitBtn);
-                
-            }
+            addQuestionsData(questions);
             
         }
     }
@@ -76,17 +40,71 @@ function getQuestions() {
 }
 
 
+function addQuestionsData(questions) {
+    for (let i = 0; i < questions.length; i++) {
+        // Create Question Box
+        let questionBox = document.createElement("div");
+        questionBox.classList.add("question");
+        // Create Question Number
+        let questionNumber = document.createElement("div");
+        questionNumber.classList.add("question-number");
+        questionNumber.innerHTML = `Question ${i+1}`
+        // Append Question Number To Question Box
+        questionBox.appendChild(questionNumber);
+        // Create Question Title
+        let questionTitle = document.createElement("div");
+        let questionTitleText = document.createTextNode(questions[i].title);
+        questionTitle.appendChild(questionTitleText);
+        questionTitle.classList.add("question-title");
+        // Append Question Title To Question Box
+        questionBox.appendChild(questionTitle);
+        // -------------------------- Answers -------------------
+        for (let j = 1; j <= 4; j++) {
+            // Create Answer Container
+            let questionAnswer = document.createElement("div");
+            // Give Every answer a dataset containing the question's right answer
+            questionAnswer.dataset.answer = questions[i].right_answer;
+            questionAnswer.classList.add("question-answer");
+            // Create Radio Input
+            let radioInput = document.createElement("input");
+            radioInput.setAttribute("type","radio");
+            // Verify ------------------------------------------------------------------------
+            radioInput.setAttribute("data-id",`answer_${j}`);
+            radioInput.name = `question_${i}`;
+            
+            let inputLabel = document.createElement("label");
+            inputLabelText = document.createTextNode(questions[i][`answer_${j}`]);
+            inputLabel.appendChild(radioInput);
+            inputLabel.appendChild(inputLabelText);
+            questionAnswer.appendChild(inputLabel);
+            questionBox.appendChild(questionAnswer); 
+        }
+
+        // Append Question Box to Container before Submit BTN
+
+        container.insertBefore(questionBox,submitBtn);
+        
+    }
+}
+
+
+
+// Submit Button ON CLICK
 submitBtn.addEventListener("click",function () {
+    // Selecting all answers
     let allAnswers = document.getElementsByClassName("question-answer");
+    // Selecting user's Answers
     let userAnswers = document.querySelectorAll(".question-answer:has(input:checked)");
     
-
+    // Marking all module right answers green
     for (let i = 0; i < allAnswers.length; i++) {
         if (allAnswers[i].innerText == allAnswers[i].dataset.answer){
-            allAnswers[i].style.borderColor = "#8cd68a";
+            allAnswers[i].style.borderColor = "#50e64a";
         }
         
     }
+
+    // if user selected the right answer increase count by 1 , else , mark answer red 
 
     for (let i = 0; i < userAnswers.length; i++) {
 
@@ -94,10 +112,12 @@ submitBtn.addEventListener("click",function () {
             grade++;
         }
         else{
-            userAnswers[i].style.borderColor = "red";
+            userAnswers[i].style.borderColor = "#e03838";
         }
         
     }
+
+    //  Display Result
     
     gradeBox.style.display = "block";
     gradeBox.firstElementChild.innerText = grade;
@@ -124,7 +144,7 @@ function countdown(duration) {
   
         if (--duration < 0) {
           clearInterval(countdownInterval);
-          submitButton.click();
+          submitBtn.click();
         }
       }, 1000);
     
